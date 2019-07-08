3e453a6ad8d0a26a4ec51dcf34503fca3a6a1906
Tomasz Chimka <tomasz.chimka@gmail.com>
2019-07-08 14:29:07 +0200

    Script_papier

--- js/script_papier.js
+++ js/script_papier.js
@@ -1,3 +1,20 @@
-var playerMove;
-playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else {
+  computerMove = 'nieznany ruch';
+}
+if (randomNumber == '2') {
+  computerMove = 'papier';
+} else {
+  computerMove = 'nieznany ruch';
+}
+if (randomNumber == '3') {
+  computerMove = 'nożyczki';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
+Step JavaScript
